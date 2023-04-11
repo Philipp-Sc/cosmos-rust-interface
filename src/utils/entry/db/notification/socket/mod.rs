8cd9e111456309ee2dc65ee7efa530a094f665c9
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-04-12 00:54:06 +0700

    logging

--- src/utils/entry/db/notification/socket/mod.rs
+++ src/utils/entry/db/notification/socket/mod.rs
@@ -31,7 +31,7 @@ impl Handler for NotificationHandler
     }
 }
 pub fn client_send_notification_request(socket_path: &str, request: CosmosRustServerValue) -> anyhow::Result<NotifyResult> {
-    info!("Sending notification request to Notification service at '{}': {:?}", socket_path, &request);
+    info!("Sending notification request to Notification service at '{}': {:?}", socket_path, &request.key());
     client_send_request(socket_path,request)
 }
 
